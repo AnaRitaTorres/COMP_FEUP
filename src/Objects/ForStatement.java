0da3e738ec58381c7610b49ecfa0268dd195a45e
AnaRitaTorres <anaritatorres@outlook.pt>
2017-05-04 14:36:04 +0100

    for statement started

--- src/Objects/ForStatement.java
+++ src/Objects/ForStatement.java
@@ -6,8 +6,32 @@ import java.util.ArrayList;
 
 public class ForStatement extends Expression{
 
-    private AssignmentExpression init;
-    private BinaryExpression test;
-    private UpdateExpression update;
+
+    //for(var i=0; i < 10;i++)
+
+    private Expression init;
+    private Expression test;
+    private Expression update;
+
+    public void print() {
+        System.out.print("for(");
+        //i=0
+        if(init.getClass().equals(AssignmentExpression.class)) {
+            init.print();
+        }
+        //var i=0
+        else if(init.getClass().equals(VariableDeclaration.class)){
+        }
+        System.out.println(";");
+        //i < 10
+        if(test.getClass().equals(BinaryExpression.class))
+            test.print();
+        //i++
+        if(update.getClass().equals(UpdateExpression.class))
+            update.print();
+
+        System.out.println("){");
+
+    }
 
 }
